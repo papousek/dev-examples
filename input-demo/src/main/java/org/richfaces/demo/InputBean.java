403c8e5fbce6860ebea40b8b023a57b69a819470
papousek <jan.papousek@gmail.com>
2012-05-03 09:39:00 +0200

    autosubmitting

--- input-demo/src/main/java/org/richfaces/demo/InputBean.java
+++ input-demo/src/main/java/org/richfaces/demo/InputBean.java
@@ -1,5 +1,6 @@
 package org.richfaces.demo;
 
+import java.util.Date;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import java.util.List;
@@ -32,6 +33,10 @@ public class InputBean {
 
     public int getCounter() {
         return counter;
+    }
+
+    public Date getDate() {
+        return new Date();
     }
 
     public void increaseCounter(ValueChangeEvent event) {
